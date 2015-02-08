41b71e8caa731f50eee4d5ce0286586f311c8971
Jake Gutierrez <theborgdude@gmail.com>
2015-02-08 09:32:03 -0600

    moved stuff to updateChecker

--- SimpleLauncher/Form1.cs
+++ SimpleLauncher/Form1.cs
@@ -15,75 +15,7 @@ namespace SimpleLauncher
     public partial class stuff : Form
     {
 
-        string downloadUrl = "";
-        string newDownloadUrl = "";
-        Version curVersion = null;
-        Version newVersion = null;
-        string xmlUrl = "http://borgdude.github.io/update.xml";
-
-        public void checkLocalVersion()
-        {
-            XmlTextReader reader = new XmlTextReader("C:/Users/Borgdude/Documents/Visual Studio 2013/Projects/SimpleLauncher/SimpleLauncher/test.xml");
-            reader.MoveToContent();
-            string elementName = "";
-            if ((reader.NodeType == XmlNodeType.Element) && reader.Name == "app") 
-            {
-                while (reader.Read())
-                {
-                    if (reader.NodeType == XmlNodeType.Element)
-                    {
-                        elementName = reader.Name;
-                    }
-                    else { 
-                        if((reader.NodeType == XmlNodeType.Text) && (reader.HasValue))
-                        {
-                            switch (elementName)
-                            {
-                                case "version":
-                                    curVersion = new Version(reader.Value);
-                                    break;
-                                case "url":
-                                    downloadUrl = reader.Value;
-                                    break;
-                            }
-                        }
-                    }
-                }
-            }
-
-
-        }
-        public void checkUpdate()
-        {
-            XmlTextReader reader = new XmlTextReader(xmlUrl);
-            reader.MoveToContent();
-            string elementName = "";
-            if ((reader.NodeType == XmlNodeType.Element) && reader.Name == "app")
-            {
-                while (reader.Read())
-                {
-                    if (reader.NodeType == XmlNodeType.Element)
-                    {
-                        elementName = reader.Name;
-                    }
-                    else
-                    {
-                        if ((reader.NodeType == XmlNodeType.Text) && (reader.HasValue))
-                        {
-                            switch (elementName)
-                            {
-                                case "version":
-                                    newVersion = new Version(reader.Value);
-                                    break;
-                                case "url":
-                                    newDownloadUrl = reader.Value;
-                                    break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        updateChecker uc = new updateChecker();
 
         
 
@@ -99,15 +31,15 @@ namespace SimpleLauncher
 
         private void updateBtn_Click(object sender, EventArgs e)
         {
-            checkLocalVersion();
-            checkUpdate();
-            if (newVersion.CompareTo(curVersion) < 0) {
+            uc.checkLocalVersion();
+            uc.checkUpdate();
+            if (uc.newVersion.CompareTo(uc.curVersion) < 0) {
                 MessageBox.Show("No update available", "Simple Launcher", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
             }
             else {
                 if ((MessageBox.Show("There is an update availabe, do you want to update now?", "Simple Launcher", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == (DialogResult.Yes)));
                 {
-                    Process.Start(newDownloadUrl);
+                    Process.Start(uc.newDownloadUrl);
                 }
             }
 
--- SimpleLauncher/updateChecker.cs
+++ SimpleLauncher/updateChecker.cs
@@ -10,7 +10,77 @@ namespace SimpleLauncher
 {
     class updateChecker
     {
-     
+        string downloadUrl = "";
+        public string newDownloadUrl = "";
+        public Version curVersion = null;
+        public Version newVersion = null;
+        string xmlUrl = "http://borgdude.github.io/update.xml";
+
+        public void checkLocalVersion()
+        {
+            XmlTextReader reader = new XmlTextReader("C:/Users/Borgdude/Documents/Visual Studio 2013/Projects/SimpleLauncher/SimpleLauncher/test.xml");
+            reader.MoveToContent();
+            string elementName = "";
+            if ((reader.NodeType == XmlNodeType.Element) && reader.Name == "app")
+            {
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element)
+                    {
+                        elementName = reader.Name;
+                    }
+                    else
+                    {
+                        if ((reader.NodeType == XmlNodeType.Text) && (reader.HasValue))
+                        {
+                            switch (elementName)
+                            {
+                                case "version":
+                                    curVersion = new Version(reader.Value);
+                                    break;
+                                case "url":
+                                    downloadUrl = reader.Value;
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+
+
+        }
+        public void checkUpdate()
+        {
+            XmlTextReader reader = new XmlTextReader(xmlUrl);
+            reader.MoveToContent();
+            string elementName = "";
+            if ((reader.NodeType == XmlNodeType.Element) && reader.Name == "app")
+            {
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element)
+                    {
+                        elementName = reader.Name;
+                    }
+                    else
+                    {
+                        if ((reader.NodeType == XmlNodeType.Text) && (reader.HasValue))
+                        {
+                            switch (elementName)
+                            {
+                                case "version":
+                                    newVersion = new Version(reader.Value);
+                                    break;
+                                case "url":
+                                    newDownloadUrl = reader.Value;
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         
        
     }
