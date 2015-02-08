6b9b2cffca577baedcdac498047a5ebc8283c74d
Jake Gutierrez <theborgdude@gmail.com>
2015-02-07 20:31:52 -0600

    Test

---
+++ SimpleLauncher/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace SimpleLauncher
+{
+    partial class stuff
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.updateLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Really Simple Launcher";
+            // 
+            // startBtn
+            // 
+            this.startBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.startBtn.Location = new System.Drawing.Point(78, 117);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(127, 23);
+            this.startBtn.TabIndex = 1;
+            this.startBtn.Text = "Start Thingy";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateBtn.Location = new System.Drawing.Point(78, 146);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(127, 23);
+            this.updateBtn.TabIndex = 2;
+            this.updateBtn.Text = "Check for Updates";
+            this.updateBtn.UseVisualStyleBackColor = true;
+            this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
+            // 
+            // updateLabel
+            // 
+            this.updateLabel.AutoSize = true;
+            this.updateLabel.Location = new System.Drawing.Point(115, 172);
+            this.updateLabel.Name = "updateLabel";
+            this.updateLabel.Size = new System.Drawing.Size(48, 13);
+            this.updateLabel.TabIndex = 3;
+            this.updateLabel.Text = "[Update]";
+            // 
+            // stuff
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.updateLabel);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.label1);
+            this.Name = "stuff";
+            this.Text = "Simple Launcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button updateBtn;
+        private System.Windows.Forms.Label updateLabel;
+    }
+}
+
---
+++ SimpleLauncher/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace SimpleLauncher
+{
+    public partial class stuff : Form
+    {
+        public stuff()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://borgdude.github.io");
+        }
+
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
